1025709053b0abe2533fc3c46e0bb24e2ff3d8d7
jparvanov-netizen <j.parvanov@gmail.com>
2026-02-06 10:45:51 +0100

    2

--- conflict_test.R
+++ conflict_test.R
@@ -10,10 +10,10 @@ data("mtcars")
 # Data manipulation: Add a factor column for cylinders
 mtcars <- mtcars %>%
   mutate(cyl_factor = factor(cyl, levels = c(4, 6, 8), labels = c("4 Cyl", "6 Cyl", "8 Cyl")))
-
-# Summarize the dataset
-summary_table <- mtcars %>%
-  group_by(cyl_factor) %>%
-  summarise(mean_mpg = mean(mpg, na.rm = TRUE))
-
+summary_table <- aggregate(
+  mpg ~ cyl_factor,
+  data = mtcars,
+  FUN = mean,
+  na.rm = TRUE
+)
 print(summary_table)
